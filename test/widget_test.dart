bf52623c961adef85d13990883e2e6e63072b47a
aleale2121 <alefewyimer4@gmail.com>
2022-03-05 08:42:21 +0300

    fix:make the test fail

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -3,10 +3,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  testWidgets('MyWidget with a title and message', (WidgetTester tester) async {
+  testWidgets('Simple Test', (WidgetTester tester) async {
     await tester.pumpWidget(MyApp());
 
-    final addButton = find.byType(FloatingActionButton);
+    final addButton = find.byType(Stepper);
     expect(addButton, findsOneWidget);
   });
 }
